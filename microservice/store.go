439224fcdf8612e2379c1c789ba2d49d3bf5e227
PierreBou <pierre.bouriat@outlook.fr>
2023-01-13 00:21:47 +0100

    sync map and basic api

--- microservice/store.go
+++ microservice/store.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"errors"
+	"io"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
 )
 
-var store = make(map[string]string)
+var store = new(sync.Map)
 
 var ErrorNoSuchKey = errors.New("no such key")
 
 func Put(key string, value string) error {
-	store[key] = value
+	store.Store(key, value)
 
 	return nil
 }
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	value, ok := store.Load(key)
 
 	if !ok {
 		return "", ErrorNoSuchKey
 	}
 
-	return value, nil
+	return value.(string), nil
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	store.Delete(key)
 	return nil
 }
 
@@ -35,8 +39,69 @@ func HelloGoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello net/http!\n"))
 }
 
-func main() {
-	http.HandleFunc("/", HelloGoHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello blabla\n"))
+}
 
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
+}
+
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	err := Delete(key)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", helloMuxHandler)
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+	r.HandleFunc("/v1/{key}", keyValueDeleteHandler).Methods("DELETE")
+
+	// http.HandleFunc("/", HelloGoHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
