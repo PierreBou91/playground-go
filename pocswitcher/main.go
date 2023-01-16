351a564ef9e6233435c97ff2fecf39280a2ee9a7
Pierre <pierre.bouriat@outlook.fr>
2023-01-16 14:20:27 +0100

    only 1 ch

--- pocswitcher/main.go
+++ pocswitcher/main.go
@@ -3,24 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	ch1 := make(chan int, 1)
-	ch2 := make(chan int, 1)
-	ch3 := make(chan int, 1)
-	ch4 := make(chan int, 1)
+	ch := make(chan int, 1)
 
 	go func() {
 		for {
-			select {
-			case <-ch1:
+			a := <-ch
+			switch a {
+			case 1:
 				fmt.Println("2")
-				ch2 <- 1
-			case <-ch2:
+				ch <- 2
+			case 2:
 				fmt.Println("3")
-				ch3 <- 1
-			case <-ch3:
+				ch <- 3
+			case 3:
 				fmt.Println("4")
-				ch4 <- 1
-			case <-ch4:
+				ch <- 4
+			case 4:
 				fmt.Println("5")
 			}
 		}
@@ -29,15 +27,6 @@ func main() {
 	var step int
 	for {
 		fmt.Scan(&step)
-		switch step {
-		case 1:
-			ch1 <- 1
-		case 2:
-			ch2 <- 1
-		case 3:
-			ch3 <- 1
-		case 4:
-			ch4 <- 1
-		}
+		ch <- step
 	}
 }
