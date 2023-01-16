2fd518d3aacffe07af387de4edc081026b08e609
Pierre <pierre.bouriat@outlook.fr>
2023-01-16 15:05:14 +0100

    optim multithread

--- pocswitcher/main.go
+++ pocswitcher/main.go
@@ -1,19 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ch := make(chan int, 1)
+	var step int
 
 	go func() {
 		for {
-			a := <-ch
+			fmt.Scan(&step)
+			ch <- step
+		}
+	}()
+
+	for a := <-ch; ; a = <-ch {
+		go func(a int) {
 			switch a {
 			case 1:
 				fmt.Println("2")
 				ch <- 2
 			case 2:
 				fmt.Println("3")
+				time.Sleep(1 * time.Second) // to confirm multiple goroutines
 				ch <- 3
 			case 3:
 				fmt.Println("4")
@@ -21,12 +32,6 @@ func main() {
 			case 4:
 				fmt.Println("5")
 			}
-		}
-	}()
-
-	var step int
-	for {
-		fmt.Scan(&step)
-		ch <- step
+		}(a)
 	}
 }
